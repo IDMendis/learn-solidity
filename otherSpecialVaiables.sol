481f28d8f02c304c05501c412f77bf940c38ad17
IDMendis <dinushimendisxp@gmail.com>
2025-10-23 21:55:26 +0530

    sol functions

--- otherSpecialVaiables.sol
+++ otherSpecialVaiables.sol
@@ -3,6 +3,27 @@ pragma solidity >= 0.8.0 <0.9.0;
 
 //your first contract
 Contract SimpleContract{
+    //string
+    string name = "john";
+    //integers
+    uint data1= 78;
+    //boolean
+    bool iscorrect = false;
+    //address
+    address walletaddress = 0x733rfv;
+    //arrays
+    string[] names;
+    //struct to define
+    struct User{
+        string name;
+        string agr;
+        string village;
+    }
+    //enums
+    enum userType {buyer, seller}
+    //mapping
+    apping(addres => uint ) public balances;
+
     //block
     block.number;
     block.difficulty;
